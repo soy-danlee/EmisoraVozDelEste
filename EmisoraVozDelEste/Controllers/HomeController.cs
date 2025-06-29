1c391d1546e1dcc5c91425ae59abb6ba9db8b68b
Danlee García Periche <124544761+soy-danlee@users.noreply.github.com>
2025-06-28 23:23:05 -0300

    arregle la pagina de inicio y puse las cotizaciones ahi

--- EmisoraVozDelEste/Controllers/HomeController.cs
+++ EmisoraVozDelEste/Controllers/HomeController.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using EmisoraVozDelEste.Models.Api;
+using EmisoraVozDelEste.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,23 +13,65 @@ namespace EmisoraVozDelEste.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        private VozDelEsteEntities1 db = new VozDelEsteEntities1();
+
+        public async Task<ActionResult> Index()
         {
-            return View();
+            var modelo = new HomePageViewModel();
+
+            // --- Clima ---
+            try
+            {
+                string ciudad = "Maldonado,UY";
+                string apiKeyClima = "99a62615b949cb6d8a96f76f97c2ff69";
+                string urlPronostico = $"https://api.openweathermap.org/data/2.5/forecast?q={ciudad}&appid={apiKeyClima}&units=metric&lang=es";
+
+                using (var client = new WebClient())
+                {
+                    var jsonPronostico = client.DownloadString(urlPronostico);
+                    var clima = ClimaOnline.FromJson(jsonPronostico);
+                    var hoy = DateTime.Now.Date;
+
+                    var primerPronosticoHoy = clima.List
+                        .Where(x => x.DtTxt.Date == hoy)
+                        .OrderBy(x => x.DtTxt)
+                        .FirstOrDefault();
+
+                    if (primerPronosticoHoy != null)
+                    {
+                        modelo.ClimaSidebar = new Clima
+                        {
+                            Fecha = primerPronosticoHoy.DtTxt,
+                            Temperatura = (decimal)primerPronosticoHoy.Main.Temp,
+                            Descripcion = primerPronosticoHoy.Weather[0].Description,
+                            Icono = primerPronosticoHoy.Weather[0].Icon
+                        };
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                modelo.ClimaSidebar = null;
+            }
+
+            // --- Cotizaciones ---
+            try
+            {
+                var cotizacionesRecientes = db.Cotizaciones
+                    .Where(c => DbFunctions.TruncateTime(c.Fecha) == DateTime.Today)
+                    .ToList();
+
+                modelo.Cotizaciones = cotizacionesRecientes;
+            }
+            catch
+            {
+                modelo.Cotizaciones = new List<Cotizaciones>();
+            }
+
+            return View(modelo);
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+        public ActionResult About() => View();
+        public ActionResult Contact() => View();
     }
 }
---
+++ EmisoraVozDelEste/Models/Api/HomePageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmisoraVozDelEste.Models.Api
+{
+    public class HomePageViewModel
+    {
+        public Clima ClimaSidebar { get; set; }
+        public List<Cotizaciones> Cotizaciones { get; set; }
+
+    }
+}
